e27e964c6936afc2f3302b68a3e94bacfcdd4599
Colin Webb <colin@insemblegroup.com>
2020-05-14 12:51:34 -0700

    broke up google activity and nearby search into request and responses

--- backend/scripts/FindRestaurantDetails.py
+++ backend/scripts/FindRestaurantDetails.py
@@ -13,6 +13,44 @@ REGEX_24_HOURS = r'\[\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\,\d+\
 REGEX_ADDRESS = r'[\\\\+\w+\'?\s+]+\,[\\+\w+\'?\s+]+\,[\w+\s+]+\,[\w+\s+]+\, United States'
 GOOG_KEY = "your google api key"
 
+def get_google_activity(name, address):
+    # returns the google activity graph for a search of an establishment
+    url, headers = build_google_activity_request(name, address)
+    return parse_google_activity(requests.get(url, headers=headers))
+
+def build_google_activity_request(name, address):
+    # builds the google activity request params
+
+    formatted_input = format_search(name, address)
+    url = 'https://www.google.com/search?q=' + formatted_input
+    return url, HEADERS
+
+def parse_google_activity(response):
+    # returns the google activity graph for a search of an establishment, based on google maps response
+
+    html_text = response.text
+    # find the 18 or 24 hour activity distribution, depending on which is present
+    data = [ast.literal_eval(item) for item in re.findall(REGEX_18_HOURS, html_text)]
+    if len(data) == 0:
+        data = [ast.literal_eval(item) for item in re.findall(REGEX_24_HOURS, html_text)]
+    return data
+
+def get_nearby(venue_type, lat, lng):
+    # returns a set of nearby venue addresses
+    url, headers = build_nearby_request(venue_type, lat, lng)
+    return parse_nearby(requests.get(url, headers=headers))
+
+def build_nearby_request(venue_type, lat, lng):
+    # returns params for which to scrape nearby
+    venue_type = venue_type.replace(" ", "+")
+    ZOOM = 17
+    url = 'https://www.google.com/maps/search/{}/@{},{},{}z'.format(venue_type, lat, lng, ZOOM)
+    return url, HEADERS
+
+def parse_nearby(response):
+    # returns a set of nearby venue addresses, based on google maps nearby search response\
+    return set(re.findall(REGEX_ADDRESS, response.text))
+
 def parse_opentable_result(response):
     """
     Parses http request response from opentable search of restaurant or store
@@ -109,7 +147,20 @@ def parse_opentable_result(response):
 
     return store
 
+def find_restaurant_details(name, address):
+    # returns the opentable details for a restaurant search
+
+    lat, lng = get_lat_lng(name, address)
+    date = today_formatted()
+    formatted_name = name.replace(" ", "+")
+    url = 'https://www.opentable.com/s/?currentview=list&size=100&sort=PreSorted&term=' + formatted_name + '&source=dtp-form&covers=2&dateTime=' + date + '&latitude=' + str(lat) + '&longitude=' + str(lng)
+
+    resp = requests.get(url, headers=HEADERS)
+    return parse_opentable_result(resp)
+
+#### Util type functions ####
 def get_lat_lng(name, address):
+    # TODO: avoid using google keys to get lat/lng
     # TODO: hide keys and save searches
 
     formatted_input = format_search(name, address)
@@ -130,37 +181,6 @@ def get_one_int_from_str(text):
 
 def get_one_float_from_str(text):
     return float(re.search(r'\d+\.\d+', text).group())
-
-def find_restaurant_details(name, address):
-    # returns the opentable details for a restaurant search
-
-    lat, lng = get_lat_lng(name, address)
-    date = today_formatted()
-    formatted_name = name.replace(" ", "+")
-    url = 'https://www.opentable.com/s/?currentview=list&size=100&sort=PreSorted&term=' + formatted_name + '&source=dtp-form&covers=2&dateTime=' + date + '&latitude=' + str(lat) + '&longitude=' + str(lng)
-
-    resp = requests.get(url, headers=HEADERS)
-    return parse_opentable_result(resp)
-
-def get_google_activity(name, address):
-    # returns the google activity graph for a search of an establishment
-
-    formatted_input = format_search(name, address)
-    url = 'https://www.google.com/search?q='+formatted_input
-    html_text = requests.get(url, headers=HEADERS).text
-    #find the 18 or 24 hour activity distribution, depending on which is present
-    data = [ast.literal_eval(item) for item in re.findall(REGEX_18_HOURS, html_text)]
-    if len(data) == 0:
-        data = [ast.literal_eval(item) for item in re.findall(REGEX_24_HOURS, html_text)]
-    return data
-
-def get_nearby(venue_type, lat, lng):
-    # returns a set of nearby venue addresses
-    venue_type = venue_type.replace(" ", "+")
-    ZOOM = 17
-    url = 'https://www.google.com/maps/search/{}/@{},{},{}z'.format(venue_type,lat,lng,ZOOM)
-    venues = set(re.findall(REGEX_ADDRESS, requests.get(url, headers=HEADERS).text))
-    return venues
 
 if __name__ == "__main__":
     def parse_opentable_result_test():
@@ -187,5 +207,5 @@ if __name__ == "__main__":
         lng = -84.3715611
         print(get_nearby(venue_type, lat, lng))
 
-    get_nearby_test()
+    get_google_activity_test()
     #pprint(find_restaurant_details("The Capital Grille", "255 East Paces Ferry Rd NE, Atlanta, GA 30305, United States"))
