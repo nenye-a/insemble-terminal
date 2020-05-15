01b5e020e7d8bbb283c19e33049ea2fb01dc2866
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-15 20:13:44 +0700

    clickaway core ui

--- frontend/src/core-ui/index.ts
+++ frontend/src/core-ui/index.ts
@@ -1,3 +1,4 @@
+export { default as ClickAway } from './ClickAway';
 export { default as Text } from './Text';
 export { default as TouchableOpacity } from './TouchableOpacity';
 export { default as View } from './View';
