e1307bee3a0e1e2ece7d2683617671f05e3bdb5c
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-05-15 12:09:28 -0700

    add performance API and spec out performance getter

--- backend/insemble-terminal/api.py
+++ backend/insemble-terminal/api.py
@@ -26,3 +26,62 @@ class BasicAPI(generics.GenericAPIView):
         }
 
         return Response(response, status=status.HTTP_200_OK)
+
+
+class PerformanceAPI(BasicAPI):
+    """
+
+    All functions related to performance data.
+    Performance API path: /performance
+
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+
+        Retrieve the performance data for a brand/category & location scope.
+
+        Parameters: {
+            location: {
+                type: 'ADDRESS'|'CITY'|'COUNTY'|'STATE'|'NATION'
+                params: string          
+            }
+            business: {
+                type: 'BUSINESS' | 'CATEGORY'
+                params: string          
+            }
+            dataType: 'BRAND'|'CATEGORY'|'OVERALL'|'ADDRESS'|'CITY'|'STATE'
+        }
+
+        Response: {
+            performance: {
+                id: prismaID                
+                createdAt: Date,
+                updatedAt: Date,
+                serachTag: {
+                    location: {
+                        type: 'ADDRESS'|'CITY'|'COUNTY'|'STATE'|'NATION'
+                        params: string                                              
+                    }
+                    business: {
+                        type: 'BUSINESS' | 'CATEGORY'
+                        params: string                                              
+                    }
+                }
+                dataType: 'BRAND'|'CATEGORY'|'OVERALL'|'ADDRESS'|'CITY'|'STATE'     
+                data: [
+                    {
+                        name: string,
+                        salesVolumeIndex?: number,
+                        avgRating?: number,
+                        avgReviews?: number,
+                        numLocations?: number,
+                        location?: number,
+                    }
+                ]
+            }
+        }
+
+        """
+
+        return Response({}, status=status.HTTP_200_OK)
