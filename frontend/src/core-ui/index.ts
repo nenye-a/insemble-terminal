f73ec9362f946c485d1afcfce1b91ffeff372261
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-18 10:18:58 +0700

    label core ui

--- frontend/src/core-ui/index.ts
+++ frontend/src/core-ui/index.ts
@@ -1,4 +1,5 @@
 export { default as ClickAway } from './ClickAway';
+export { default as Label } from './Label';
 export { default as Text } from './Text';
 export { default as TouchableOpacity } from './TouchableOpacity';
 export { default as View } from './View';
