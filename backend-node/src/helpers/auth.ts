cd9f9b514e37acff9769b35b2ea39921795bfdce
RaymondAnggara <raymond@kodefox.com>
2020-05-18 14:07:46 +0700

    Add helper for auth

---
+++ backend-node/src/helpers/auth.ts
@@ -0,0 +1,37 @@
+import { prisma } from '../prisma';
+import getRandomBytes from './getRandomBytes';
+import { User } from '@prisma/client';
+
+export async function authSession(authToken: string | undefined) {
+  let [sessionID, sessionToken] = authToken ? authToken.split(':') : [];
+  if (!sessionID || !sessionToken) {
+    return null;
+  }
+  let session = await prisma.userSession.findOne({
+    where: { id: sessionID },
+    include: { user: true },
+  });
+  if (!session) {
+    return null;
+  }
+  let { user, token } = session;
+
+  if (token !== sessionToken) {
+    return null;
+  }
+  return {
+    userId: user.id,
+  };
+}
+
+export async function createSession(user: User) {
+  let bytes = await getRandomBytes(18);
+  let sessionData = {
+    data: {
+      token: bytes.toString('base64'),
+      user: { connect: { id: user.id } },
+    },
+  };
+  let session = await prisma.userSession.create(sessionData);
+  return session.id + ':' + session.token;
+}
---
+++ backend-node/src/helpers/sendEmail.ts
@@ -0,0 +1,49 @@
+import sgMail from '@sendgrid/mail';
+import fs from 'fs';
+import ejs from 'ejs';
+
+import { SENDGRID_API_KEY } from '../constants/constants';
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+let emailVerificationTemplate = fs
+  .readFileSync('src/emailTemplates/emailVerification.html')
+  .toString();
+
+async function sendVerificationEmail(
+  receiver: { email: string; name: string },
+  link: string,
+) {
+  let htmlContent = ejs.render(emailVerificationTemplate, {
+    name: receiver.name,
+    url: link,
+  });
+  let msg = {
+    to: receiver.email,
+    from: 'Insemble <no-reply@insemble.co>',
+    subject: 'Verify your email with Insemble Terminal',
+    text: `Please verify your email by clicking this link ${link}`,
+    html: htmlContent,
+  };
+  await sgMail.send(msg);
+}
+
+async function sendForgotPasswordEmail(
+  receiver: { email: string; name: string },
+  link: string,
+) {
+  let htmlContent = ejs.render(emailVerificationTemplate, {
+    name: receiver.name,
+    url: link,
+  });
+  let msg = {
+    to: receiver.email,
+    from: 'Insemble <no-reply@insemble.co>',
+    subject: 'Forgot password user Insemble Terminal',
+    text: `We received a request to reset the password directed to this e-mail address. Please proceed by clicking this link : ${link}`,
+    html: htmlContent,
+  };
+  await sgMail.send(msg);
+}
+
+export { sendVerificationEmail, sendForgotPasswordEmail };
