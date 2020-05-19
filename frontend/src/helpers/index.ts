b962efd6fcc6f525893847f76332ac25a081b6f3
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-19 22:51:42 +0700

    add some helper

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -1,3 +1,5 @@
 export { default as capitalize } from './capitalize';
+export { default as isLocalUrl } from './isLocalUrl';
 export { default as useID } from './useID';
 export { default as useGoogleMaps } from './useGoogleMaps';
+export * from './validation';
