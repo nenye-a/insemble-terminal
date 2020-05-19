5c2a0e3f44de146743a43404255562e42b050ecd
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-05-19 18:02:53 +0700

    [Setup] Apollo (#26)

---
+++ frontend/src/graphql/apolloClient.ts
@@ -0,0 +1,34 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+import { API_URI } from '../constants/uris';
+
+const cache = new InMemoryCache();
+
+const errorLink = onError((_) => {
+  // TODO: handle error
+});
+
+const httpLink = new HttpLink({
+  uri: API_URI,
+});
+
+function initApolloClient() {
+  cache.writeData({
+    data: {
+      // default state
+    },
+  });
+  const client = new ApolloClient({
+    link: ApolloLink.from([errorLink, httpLink]),
+    resolvers: {},
+    cache,
+  });
+  return client;
+}
+let apolloClient = initApolloClient();
+
+export default apolloClient;
