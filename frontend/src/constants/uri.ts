aebf9e545af034ec3611eb48aff08fb0e1982ffe
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-05-20 20:41:20 +0700

    [Scene, Connect] Result (#42)

--- frontend/src/constants/uri.ts
+++ frontend/src/constants/uri.ts
@@ -4,4 +4,6 @@ const MAPS_API_KEY =
 export const API_URI =
   process.env.REACT_APP_API || 'https://terminal-node-backend.herokuapp.com/';
 
+export const API_URI = process.env.REACT_APP_API || 'http://localhost:4000';
+
 export const MAPS_URI = `https://maps.googleapis.com/maps/api/js?key=${MAPS_API_KEY}&libraries=places`;
