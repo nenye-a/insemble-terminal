8e98edcd795385e8d04e299efa0721451ec54d68
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-20 12:21:31 +0700

    hide and show navbar

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -5,10 +5,11 @@ import { ResultsScene } from '../scenes';
 export type RouteType = Omit<RouteProps, 'component'> & {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   component: React.ComponentType<any>;
+  showHeader?: boolean;
 };
 
 const ROUTES: Array<RouteType> = [
-  { path: '/', exact: true, component: ResultsScene },
+  { path: '/', exact: true, component: ResultsScene, showHeader: false },
 ];
 
 export default ROUTES;
--- frontend/src/scenes/results/ResultsScene.tsx
+++ frontend/src/scenes/results/ResultsScene.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { View, Text } from '../../core-ui';
+import HeaderNavigationBar from '../../components/HeaderNavigationBar';
 
 export default function ResultsScene() {
   return (
     <View>
+      <HeaderNavigationBar />
       <Text>Result Scene</Text>
     </View>
   );
