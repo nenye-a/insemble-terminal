119ed9b60ef1e956a200411e38a06f5950f0027d
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-20 19:14:35 +0700

    remove unused

--- frontend/src/fixtures/dummyData.ts
+++
@@ -1,6 +0,0 @@
-export const BUSINESS_CATEGORY_DATA = [
-  { id: '1', type: 'BUSINESS', params: 'KFC' },
-  { id: '2', type: 'CATEGORY', params: 'Cake' },
-  { id: '3', type: 'BUSINESS', params: 'McD' },
-  { id: '4', type: 'BUSINESS', params: 'The Cheesecake Factory' },
-];
