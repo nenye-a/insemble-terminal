5307904a468225681764cb02dac4a34d1a6bb5f0
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-05-21 20:53:27 -0700

    remove TODO

--- backend/data/news.py
+++ backend/data/news.py
@@ -42,8 +42,6 @@ NEWS_TERMS = ["retail news", "commercial real estate news", "closings", "opening
 
 
 def news(business=None, location=None):
-    # TODO: make take in time to convert news items to show items
-    # are X minutes/hours ago instead of just the day.
 
     news_search_terms = [" ".join([location, news_term]) for news_term in
                          NEWS_TERMS] if location else NEWS_TERMS.copy()
