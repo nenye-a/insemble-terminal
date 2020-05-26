bb94592b0e66145c46f4491bc79196d79ea04f3b
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-26 13:32:31 +0700

    add pin and add icon

--- frontend/src/scenes/results/ResultTitle.tsx
+++ frontend/src/scenes/results/ResultTitle.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { View, Text } from '../../core-ui';
+import { View, Text, TouchableOpacity } from '../../core-ui';
 import { THEME_COLOR } from '../../constants/colors';
 import { FONT_SIZE_LARGE, FONT_WEIGHT_BOLD } from '../../constants/theme';
+import SvgPin from '../../components/icons/pin';
+import SvgRoundAdd from '../../components/icons/round-add';
 
 type Props = {
   title: string;
@@ -14,7 +16,14 @@ export default function ResultTitle(props: Props) {
   return (
     <Container>
       <Title>{title}</Title>
-      {/* TODO: add compare & pin icon */}
+      <Row>
+        <Touchable>
+          <SvgRoundAdd />
+        </Touchable>
+        <Touchable>
+          <SvgPin />
+        </Touchable>
+      </Row>
     </Container>
   );
 }
@@ -22,10 +31,26 @@ export default function ResultTitle(props: Props) {
 const Container = styled(View)`
   padding: 8px 0;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Title = styled(Text)`
   color: ${THEME_COLOR};
   font-size: ${FONT_SIZE_LARGE};
   font-weight: ${FONT_WEIGHT_BOLD};
+`;
+
+const Row = styled(View)`
+  flex-direction: row;
+`;
+
+const Touchable = styled(TouchableOpacity)`
+  margin-left: 12px;
+  svg {
+    color: ${THEME_COLOR};
+    &:hover {
+      opacity: 0.7;
+    }
+  }
 `;
