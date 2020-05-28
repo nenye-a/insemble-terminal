37d33af5f5d1abcbb228e6ebf8627d93ae8d1aaf
RaymondAnggara <raymond@kodefox.com>
2020-05-28 18:52:08 +0700

    Add user profile query

--- backend-node/src/resolvers/index.ts
+++ backend-node/src/resolvers/index.ts
@@ -1,6 +1,7 @@
 export * from './auth/loginMutation';
 export * from './auth/registerMutation';
 export * from './auth/userVerificationQuery';
+export * from './auth/userProfileQuery';
 
 export * from './search/searchMutation';
 export * from './compare/updateComparisonMutation';
