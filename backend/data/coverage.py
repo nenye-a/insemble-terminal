07ae891341df5b229b5c845898fbc4c1a4e03564
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-05-28 16:10:26 -0700

    implement categorical coverage

--- backend/data/coverage.py
+++ backend/data/coverage.py
@@ -41,8 +41,57 @@ def coverage(name, location, scope):
         'num_locations': locations['num_locations'],
         'coverage': [{
             'business_name': name,
+            'num_locations': locations['num_locations'],
             'locations': locations['locations']
         }]
+    }
+
+
+def category_coverage(category, location, scope):
+
+    if scope.lower() == 'city':
+        matching_places = list(utils.DB_TERMINAL_PLACES.find({
+            'type': {"$regex": r"^" + utils.modify_word(category), "$options": "i"},
+            'city': {"$regex": r"^" + utils.modify_word(location[:5]), "$options": "i"},
+        }))
+    elif scope.lower() == 'county':
+        region = utils.DB_REGIONS.find_one({
+            'name': {"$regex": r"^" + utils.modify_word(location)},
+            'type': 'county'
+        })
+        if not region:
+            return None
+        matching_places = list(utils.DB_TERMINAL_PLACES.find({
+            'type': {"$regex": r"^" + utils.modify_word(category), "$options": "i"},
+            'location': {'$geoWithin': {'$geometry': region['geometry']}},
+        }))
+    else:
+        return None
+
+    if not matching_places:
+        return None
+
+    coverage_data = categorical_data(matching_places)
+
+    return {
+        'name': utils.modify_word(category),
+        'num_locations': coverage_data['num_locations'],
+        'coverage': coverage_data['coverage']
+    }
+
+
+def categorical_data(list_places):
+    brand_dict = performance.section_by_brand(list_places)
+    coverage_dict = {k: extract_coverage(v) for k, v in brand_dict.items()}
+    coverage = [{
+        'busines_name': brand,
+        'num_locations': locations['num_locations'],
+        'locations': locations['locations']
+    } for brand, locations in coverage_dict.items()]
+
+    return {
+        'num_locations': sum(brand['num_locations'] for brand in coverage),
+        'coverage': coverage
     }
 
 
@@ -69,6 +118,12 @@ def extract_coverage(list_places):
 
 if __name__ == "__main__":
     def test_coverage():
+        print(coverage("Starbucks", "Los Angeles", "City"))
         print(coverage("Starbucks", "Los Angeles", "County"))
 
-    test_coverage()
+    def test_category_coverage():
+        # print(category_coverage("Mexican Restaurant", "Los Angeles", "City"))
+        print(category_coverage("Mexican Restaurant", "Los Angeles County", "County"))
+
+    # test_coverage()
+    test_category_coverage()
