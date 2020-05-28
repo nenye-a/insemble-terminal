aae7a82c794db179b9f539455a2dface4bf7781e
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-05-28 18:00:21 +0700

    adjust result query

--- frontend/src/graphql/queries/server/results.ts
+++ frontend/src/graphql/queries/server/results.ts
@@ -5,11 +5,13 @@ export const GET_PERFORMANCE_TABLE_DATA = gql`
     $performanceType: PerformanceTableType!
     $businessTagId: String
     $locationTagId: String
+    $tableId: String
   ) {
     performanceTable(
       performanceType: $performanceType
       businessTagId: $businessTagId
       locationTagId: $locationTagId
+      tableId: $tableId
     ) {
       id
       type
