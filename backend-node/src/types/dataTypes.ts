0e37808fed8d67bf514108395d4f534fa2521675
RaymondAnggara <raymond@kodefox.com>
2020-05-29 16:14:03 +0700

    Python Type def

--- backend-node/src/types/dataTypes.ts
+++ backend-node/src/types/dataTypes.ts
@@ -12,3 +12,18 @@ export type PyPerformanceResponse = {
   dataType: 'BRAND' | 'CATEGORY' | 'OVERALL' | 'ADDRESS' | 'CITY' | 'STATE';
   data: Array<PyPerformanceData>;
 };
+
+export type PyNewsData = {
+  title?: string;
+  link?: string;
+  published?: string;
+  source?: string;
+  description?: string;
+  relevance?: number;
+};
+
+export type PyNewsResponse = {
+  createdAt: Date;
+  updatedAt: Date;
+  data: Array<PyNewsData>;
+};
