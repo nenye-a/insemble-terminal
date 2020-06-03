26d56dc47070e52639a89e1a582c41f82f8be4c7
RaymondAnggara <raymond@kodefox.com>
2020-06-03 16:56:59 +0700

    add performance and ownership type on pinnedFeed return

--- backend-node/src/typeSchemas/PinnedFeed.ts
+++ backend-node/src/typeSchemas/PinnedFeed.ts
@@ -1,4 +1,6 @@
 import { objectType } from 'nexus';
+import { prisma } from '../prisma';
+import { PerformanceType, OwnershipType } from '@prisma/client';
 
 export let PinnedFeed = objectType({
   name: 'PinnedFeed',
@@ -6,5 +8,51 @@ export let PinnedFeed = objectType({
     t.model.id();
     t.model.tableId();
     t.model.tableType();
+    t.field('performanceTableType', {
+      type: 'PerformanceTableType',
+      resolve: async ({ tableType, tableId }) => {
+        let performanceTableType: PerformanceType | null = null;
+        if (tableType === 'PERFORMANCE') {
+          let performance = await prisma.performance.findOne({
+            where: {
+              id: tableId,
+            },
+          });
+          if (performance) {
+            performanceTableType = performance.type;
+          }
+        }
+        return performanceTableType;
+      },
+      nullable: true,
+    });
+    t.field('ownershipTableType', {
+      type: 'OwnershipType',
+      resolve: async ({ tableType, tableId }) => {
+        let ownershipType: OwnershipType | null = null;
+        if (tableType === 'OWNERSHIP_CONTACT') {
+          let ownershipContact = await prisma.ownershipContact.findOne({
+            where: {
+              id: tableId,
+            },
+          });
+          if (ownershipContact) {
+            ownershipType = ownershipContact.type;
+          }
+        }
+        if (tableType === 'OWNERSHIP_INFO') {
+          let ownershipInfo = await prisma.ownershipInfo.findOne({
+            where: {
+              id: tableId,
+            },
+          });
+          if (ownershipInfo) {
+            ownershipType = ownershipInfo.type;
+          }
+        }
+        return ownershipType;
+      },
+      nullable: true,
+    });
   },
 });
