26adbf8a30d6d2d4a1c8a84ff40f795b83800016
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-03 11:09:10 +0700

    ownership card

---
+++ frontend/src/scenes/results/OwnershipInformationCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { View, Text, Link } from '../../core-ui';
+import { FONT_WEIGHT_MEDIUM, FONT_SIZE_XSMALL } from '../../constants/theme';
+import { GRAY, SHADOW_COLOR, WHITE } from '../../constants/colors';
+
+type Props = {
+  name: string;
+  address: string;
+  phone: string;
+  website: string;
+  lastUpdate: string;
+};
+
+export default function OwnershipInformationCard(props: Props) {
+  let { name, address, phone, website, lastUpdate } = props;
+  return (
+    <Container>
+      <Row>
+        <LabelText>Parent Company:</LabelText>
+        <ValueText>{name}</ValueText>
+      </Row>
+      <Row>
+        <LabelText>Headquarters Address:</LabelText>
+        <ValueText>{address}</ValueText>
+      </Row>
+      <Row>
+        <LabelText>Phone:</LabelText>
+        <ValueText>{phone}</ValueText>
+      </Row>
+      <Row>
+        <LabelText>Website</LabelText>
+        <Link href={website}>{website}</Link>
+      </Row>
+      <LastUpdateText>Last Updated: {lastUpdate}</LastUpdateText>
+    </Container>
+  );
+}
+
+const Container = styled(View)`
+  padding: 8px 28px;
+  background-color: ${WHITE};
+  box-shadow: ${SHADOW_COLOR};
+  width: 600px;
+`;
+
+const Row = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 8px 0;
+`;
+
+const LabelText = styled(Text)`
+  font-weight: ${FONT_WEIGHT_MEDIUM};
+  color: ${GRAY};
+`;
+
+const ValueText = styled(Text)`
+  text-align: right;
+`;
+
+const LastUpdateText = styled(Text)`
+  font-size: ${FONT_SIZE_XSMALL};
+  align-self: flex-end;
+  text-align: right;
+  margin-top: 30px;
+`;
