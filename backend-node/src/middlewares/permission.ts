e34a03cb382903ad87e8bf840d1f1cefcb03c62e
RaymondAnggara <raymond@kodefox.com>
2020-06-04 14:50:20 +0700

    Use auth permission on edit profile

--- backend-node/src/middlewares/permission.ts
+++ backend-node/src/middlewares/permission.ts
@@ -13,6 +13,7 @@ let permissions = shield({
     terminal: isUserAuthenticated,
   },
   Mutation: {
+    editUserProfile: isUserAuthenticated,
     search: isUserAuthenticated,
     createTerminal: isUserAuthenticated,
     deleteTerminal: isUserAuthenticated,
