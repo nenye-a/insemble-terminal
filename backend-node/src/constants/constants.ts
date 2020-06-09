b965b6cefe7db4ed2a2666699b4d0237643f80a8
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-06-09 01:19:15 -0700

    adjust constants for API_URI

--- backend-node/src/constants/constants.ts
+++ backend-node/src/constants/constants.ts
@@ -5,7 +5,7 @@ let SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
 let NODE_ENV = process.env.NODE_ENV || 'development';
 let HOST = process.env.HOST || 'http://localhost:4000'; // NOTES: make sure to set HOST on the env for production
 let FRONTEND_HOST = process.env.FRONTEND_HOST || 'http://localhost:4000'; // NOTES: make sure to set FRONTEND_HOST on the env for production
-let API_URI = 'http://localhost:8000';
+let API_URI = process.env.API_URI || 'http://localhost:8000';
 let CONTACT_PERSON = process.env.CONTACT_PERSON || 'sales@insemblegroup.com';
 const hostname = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1';
 const port = ((process.env.PORT as unknown) as number) || 4000;
