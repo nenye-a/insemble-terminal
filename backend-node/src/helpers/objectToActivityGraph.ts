9fe0314346ee6fe75bf2f863ce6090e6aa8b6237
Raymond Anggara <46436058+RaymondAnggara@users.noreply.github.com>
2020-06-09 16:15:06 +0700

    Make sorting activity graph to 4AM to 3AM (#201)

--- backend-node/src/helpers/objectToActivityGraph.ts
+++ backend-node/src/helpers/objectToActivityGraph.ts
@@ -1,16 +1,27 @@
+import { activityGraphSort } from '../constants/activity';
+
 export let objectToActivityGraph = (
   object: Record<string, number | undefined>,
   businessName: string,
   location?: string,
 ) => {
-  let objectKeyValue = Object.keys(object).map((key) => {
-    let amount = object[key];
-    let business = location ? `${businessName} (${location})` : businessName;
-    return {
-      name: key,
-      business,
-      amount,
-    };
-  });
+  let objectKeyValue = Object.keys(object)
+    .sort((keyA, keyB) => {
+      let positionKeyA = activityGraphSort.find(({ name }) => name === keyA);
+      let positionKeyB = activityGraphSort.find(({ name }) => name === keyB);
+      if (!positionKeyA || !positionKeyB) {
+        return 0;
+      }
+      return positionKeyA.value - positionKeyB.value;
+    })
+    .map((key) => {
+      let amount = object[key];
+      let business = location ? `${businessName} (${location})` : businessName;
+      return {
+        name: key,
+        business,
+        amount,
+      };
+    });
   return objectKeyValue;
 };
