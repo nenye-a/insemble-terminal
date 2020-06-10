6ded1e730cbe3294358f55afbacc67f09e0e55ef
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-10 13:33:55 +0700

    remove infobox from table header

--- frontend/src/scenes/results/PerformanceTable.tsx
+++ frontend/src/scenes/results/PerformanceTable.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import { View, Text } from '../../core-ui';
 import { DataTable } from '../../components';
 import {
   GetPerformanceTable_performanceTable_data as PerformanceTableData,
   GetPerformanceTable_performanceTable_compareData as PerformanceTableCompareData,
 } from '../../generated/GetPerformanceTable';
-import { TABLE_PURPLE_BACKGROUND, THEME_COLOR } from '../../constants/colors';
+import { TABLE_PURPLE_BACKGROUND } from '../../constants/colors';
 import { useSortableData } from '../../helpers';
 import { Direction } from '../../types/types';
-import { FONT_WEIGHT_BOLD } from '../../constants/theme';
-import SvgRoundAdd from '../../components/icons/round-add';
 
 type Props = {
   data: Array<PerformanceTableData>;
@@ -57,29 +53,6 @@ export default function PerformanceTable(props: Props) {
           }}
           sortConfig={sortConfig}
           name="totalSales"
-          infoboxContent={
-            <View>
-              <PopoverTitle>Ratings</PopoverTitle>
-              <Text>
-                The customer volume index represents the volume of customers
-                that this company or category sees, over the last three months.
-              </Text>
-              <Text
-                style={{
-                  marginTop: 30,
-                }}
-              >
-                Use the
-                <SvgRoundAdd
-                  width={18}
-                  height={18}
-                  style={{ marginLeft: 8, marginRight: 8, marginBottom: -4 }}
-                />
-                to compare against brand performance, location performance, or
-                industry performance.
-              </Text>
-            </View>
-          }
         >
           Customer Volume Index
         </DataTable.HeaderCell>
@@ -91,15 +64,6 @@ export default function PerformanceTable(props: Props) {
           }}
           sortConfig={sortConfig}
           name="avgRating"
-          infoboxContent={
-            <View>
-              <PopoverTitle>Ratings</PopoverTitle>
-              <Text>
-                A composite rating of scores from multiple online review
-                sources.
-              </Text>
-            </View>
-          }
         >
           Avg rating
         </DataTable.HeaderCell>
@@ -157,9 +121,3 @@ export default function PerformanceTable(props: Props) {
     </DataTable>
   );
 }
-
-const PopoverTitle = styled(Text)`
-  color: ${THEME_COLOR};
-  font-weight: ${FONT_WEIGHT_BOLD};
-  margin-bottom: 12px;
-`;
