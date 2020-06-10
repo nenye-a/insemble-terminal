d213734cc632daae5d3813fb57a3ca0941d1e09a
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-10 12:33:31 +0700

    remove react text loop

--- frontend/src/scenes/landing/LandingScene.tsx
+++ frontend/src/scenes/landing/LandingScene.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import TextLoop from 'react-text-loop';
 import { useTransition, animated } from 'react-spring';
 
 import { View, Text as BaseText, Divider } from '../../core-ui';
