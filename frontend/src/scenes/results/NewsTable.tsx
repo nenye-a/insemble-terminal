9671e65d5af15b5bfe526009426267e6065e5d04
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-11 12:20:41 +0700

    change newstable color

--- frontend/src/scenes/results/NewsTable.tsx
+++ frontend/src/scenes/results/NewsTable.tsx
@@ -5,8 +5,10 @@ import {
   getPublishedDate,
   useSortableData,
   lightenOrDarkenColor,
+  getTextColor,
 } from '../../helpers';
 import { Direction, MergedNewsData } from '../../types/types';
+import { WHITE } from '../../constants/colors';
 
 type Props = {
   data: Array<MergedNewsData>;
@@ -42,17 +44,17 @@ export default function NewsTable(props: Props) {
       </DataTable.HeaderRow>
       {sortedData.map((row, index) => {
         let { title = '', link = '', source = '', published, fill } = row;
+        let bgColor = fill ? lightenOrDarkenColor(fill, 25) : WHITE;
+        let textColor = getTextColor(bgColor);
         return (
           <DataTable.Row
             key={index}
             onPress={() => {
               window.open(link, '_blank');
             }}
-            {...(fill && {
-              style: {
-                backgroundColor: lightenOrDarkenColor(fill, 25),
-              },
-            })}
+            style={{
+              backgroundColor: bgColor,
+            }}
           >
             <DataTable.Cell
               width={500}
@@ -61,12 +63,15 @@ export default function NewsTable(props: Props) {
                 overflow: 'hidden',
                 whiteSpace: 'nowrap',
                 display: 'block',
+                color: textColor,
               }}
             >
               {title}
             </DataTable.Cell>
-            <DataTable.Cell>{source}</DataTable.Cell>
-            <DataTable.Cell align="right">
+            <DataTable.Cell style={{ color: textColor }}>
+              {source}
+            </DataTable.Cell>
+            <DataTable.Cell style={{ color: textColor }} align="right">
               {getPublishedDate(published)}
             </DataTable.Cell>
           </DataTable.Row>
