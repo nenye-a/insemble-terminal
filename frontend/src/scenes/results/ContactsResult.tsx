9e5c943899f527ecb1f4b9674a01ba8a2ffc6c12
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-15 11:16:54 +0700

    make contacts and information as a component

---
+++ frontend/src/scenes/results/ContactsResult.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+
+import { View, LoadingIndicator } from '../../core-ui';
+import { EmptyDataComponent, ErrorComponent } from '../../components';
+import {
+  ReviewTag,
+  OwnershipType,
+  TableType,
+} from '../../generated/globalTypes';
+import {
+  GetOwnershipContactData,
+  GetOwnershipContactDataVariables,
+} from '../../generated/GetOwnershipContactData';
+import { GET_OWNERSHIP_CONTACT_DATA } from '../../graphql/queries/server/results';
+import { formatErrorMessage } from '../../helpers';
+
+import ResultTitle from './ResultTitle';
+import ContactsTable from './ContactsTable';
+
+type Props = {
+  businessTagId?: string;
+  locationTagId?: string;
+  tableId?: string;
+  pinTableId?: string;
+  ownershipType: OwnershipType;
+  title: string;
+};
+
+export default function ContactsResult(props: Props) {
+  let {
+    businessTagId,
+    locationTagId,
+    tableId,
+    pinTableId,
+    ownershipType,
+    title,
+  } = props;
+  let { data, loading, error, refetch } = useQuery<
+    GetOwnershipContactData,
+    GetOwnershipContactDataVariables
+  >(GET_OWNERSHIP_CONTACT_DATA, {
+    variables: {
+      businessTagId,
+      locationTagId,
+      ownershipType,
+      tableId,
+    },
+  });
+
+  let noData =
+    !data?.ownershipContactTable.data ||
+    data?.ownershipContactTable.data.length === 0;
+
+  return (
+    <Container>
+      <ResultTitle
+        title={title}
+        noData={noData}
+        reviewTag={ReviewTag.OWNERSHIP}
+        tableId={data?.ownershipContactTable.id || ''}
+        onTableIdChange={(newTableId: string) => {
+          refetch({
+            tableId: newTableId,
+            ownershipType,
+          });
+        }}
+        canCompare={false}
+        tableType={TableType.OWNERSHIP_CONTACT}
+        {...(data?.ownershipContactTable.businessTag && {
+          businessTag: {
+            params: data.ownershipContactTable.businessTag.params,
+            type: data.ownershipContactTable.businessTag.type,
+          },
+        })}
+        {...(data?.ownershipContactTable.locationTag && {
+          locationTag: {
+            params: data.ownershipContactTable.locationTag.params,
+            type: data.ownershipContactTable.locationTag.type,
+          },
+        })}
+        pinTableId={pinTableId}
+      />
+      {loading ? (
+        <LoadingIndicator />
+      ) : error ? (
+        <ErrorComponent text={formatErrorMessage(error.message)} />
+      ) : noData ? (
+        <EmptyDataComponent />
+      ) : (
+        <ContactsTable data={data?.ownershipContactTable.data} />
+      )}
+    </Container>
+  );
+}
+
+const Container = styled(View)`
+  padding: 20px 0;
+`;
