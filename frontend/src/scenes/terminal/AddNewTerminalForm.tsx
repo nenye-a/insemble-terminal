b5e678b130c95ccfcfc6e2f2d03b23f6ba2eafff
RaymondAnggara <raymond@kodefox.com>
2020-06-15 17:34:07 +0700

    NIT

--- frontend/src/scenes/terminal/AddNewTerminalForm.tsx
+++ frontend/src/scenes/terminal/AddNewTerminalForm.tsx
@@ -62,7 +62,7 @@ export default function AddNewTerminalForm(props: Props) {
       <TextInput
         label="Description (optional)"
         containerStyle={containerStyle}
-        ref={register()}
+        ref={register}
         name="description"
       />
       <ButtonContainer>
