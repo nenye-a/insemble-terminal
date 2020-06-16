e9ca29be3a8d95a21f05e1d6d228d423a2ddd3d6
RaymondAnggara <raymond@kodefox.com>
2020-06-16 14:33:37 +0700

    OrderBy pinnedFeeds for constant position

--- backend-node/src/typeSchemas/Terminal.ts
+++ backend-node/src/typeSchemas/Terminal.ts
@@ -1,12 +1,24 @@
 import { objectType } from 'nexus';
 
+import { prisma } from '../prisma';
+
 export let Terminal = objectType({
   name: 'Terminal',
   definition(t) {
     t.model.id();
     t.model.name();
     t.model.description();
-    t.model.pinnedFeeds();
+    t.field('pinnedFeeds', {
+      type: 'PinnedFeed',
+      resolve: async ({ id }) => {
+        let pinnedFeeds = await prisma.pinnedFeed.findMany({
+          where: { terminal: { id } },
+          orderBy: { id: 'asc' },
+        });
+        return pinnedFeeds;
+      },
+      list: true,
+    });
     t.model.createdAt();
     t.model.updatedAt();
   },
