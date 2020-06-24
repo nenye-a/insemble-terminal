de2eda9eac22240a2abdf20d0af25a762052b8ce
Colin Webb <colin@insemblegroup.com>
2020-06-24 00:03:13 -0700

    sourced report for retailers and landlords

---
+++ backend/data/scripts/personal_reports.py
@@ -0,0 +1,301 @@
+# imports
+import sys
+import pandas as pd
+from collections import Counter
+sys.path.extend(['/Users/colin/Desktop/insemble-terminal/backend/data'])
+import preprocess
+import utils
+import google
+import requests
+from decouple import config
+
+SEARCH_RADIUS = 10000  # meters
+DEFAULT_BRAND = 'Starbucks'
+GOOG_KEY = config("GOOG_KEY")
+
+# report logic:
+# - Retailer with found location nearby
+#   - activity of retail location, competitor location, and category
+#   - performance of retail location, competitor location, and category
+# - Retailer with found location not nearby
+#   - attempt to choose one in closest MSA. Different messaging around retail site ("one of your locations")
+#   - activity of retail location, competitor location, and category
+#   - performance of retail location, competitor location, and category
+# - Retailer with no found location
+#   - attempt to find subsidary organization that has locations
+#   - choose well known retail site in their state that we have info for to show activity and performance
+#
+# - Landlord with location in MSA region
+#   - activity for nearby chain store compared to the general activity for that chain
+#   - performance for nearby chain store compared to the general activity for that chain
+#   - performance for a variety of nearby stores
+# - Landlord with location outside of MSA region
+#   - snap to nearest MSA region, and choose random retailers from a specific location
+#
+# - Broker with location in MSA region
+#   - performance City breakdown of 3 store types (discount store, drug store, sandwich shop)
+#   - coverage of those 3 store types in a city that has a high volume and store count across the three
+#   - could also do city & coverage separately
+# - Broker with location outside of MSA region
+#   - snap to closest MSA region and find general example using above strategy
+#
+# - Municipality
+#
+# - Services
+#
+# - Scholar
+#
+# - People Research
+#
+# - Fail to find type
+#   - default retail case
+# - Fail to find address or brand
+#   - if retailer
+
+def personal_reports(csv_filename):
+    # parse CSV of names to get name, company, address, type, conference.
+    contact_df = pd.read_csv(csv_filename)
+    all_queries = []
+
+    # iterate through the list
+    for i in contact_df.index:
+        # if we've already populated the search for this contact, skip
+
+        name = contact_df["Name"][i]
+        type = contact_df["Type"][i]
+        company = contact_df["Company"][i]
+        address = contact_df["Address"][i]
+        city = contact_df["City"][i]
+
+        print("Servicing {}. Finding locations for {}, a {} near {} {}".format(name, company, type, address, city))
+
+        query_list = logic_handler(company, address, city, type)
+
+        # TODO add result query to mongo for that (company, address, city, type)
+        all_queries.append(query_list)
+        print("{},{},{},{} Queries ----".format(company, address, city, type), query_list)
+
+    print(all_queries)
+
+def logic_handler(company, address, city, type):
+    # decides what to do given business details
+    # TODO handle none cases, including no lat lng found
+    matches = None
+    query_list = []
+    type = type.lower()
+
+    print("Gathering coordinates")
+
+    coords = get_coords(city, address)
+    if not coords:
+        coords = get_coords(city) # if coords doesn't load initially, try searching city alone
+    if not coords:
+        print("Could not retrieve coordinates for {}, {}".format(address, city))
+        return []
+
+    lat, lng = coords['lat'], coords['lng']
+    location = {"type": "Point", "coordinates": [lng, lat]}
+
+    # if type retailer
+    if 'retailer' in type:
+
+        # find closest retail site
+        print("Finding closest retail site for user location")
+        processed_brand = preprocess.preprocess(company)
+        count_documents = utils.DB_TERMINAL_PLACES.count_documents({"name": processed_brand})
+        if count_documents:
+            matches = utils.DB_TERMINAL_PLACES.find(
+                {"name":processed_brand, "location":{"$near":{"$geometry":location}}})
+
+        # if retail site is not found at all
+        if not matches:
+
+            # try finding subsidiaries
+            print("Retail site not found. Attempting to find subsidary sites")
+            subsidiaries = google.get_company(processed_brand, 'subsidiaries')
+            if subsidiaries:
+                for sub in subsidiaries:
+                    if matches or (sub.lower() == 'more'):
+                        break # break if matches or if reached the paginated end of google company subsidaries
+                    processed_sub = preprocess.preprocess(sub)
+                    count_documents = utils.DB_TERMINAL_PLACES.count_documents({"name": processed_sub})
+                    if count_documents:
+                        matches = utils.DB_TERMINAL_PLACES.find(
+                            {"name": processed_sub,
+                             "location": {"$near": {"$geometry": location}}})
+
+        # if subsidaries weren't found, find well known retailer in their area to get activity and performance from
+        print("No subsidiaries found. Attempting to find well known default retailer {} nearby".format(DEFAULT_BRAND))
+        if not matches:
+            processed_default = preprocess.preprocess(DEFAULT_BRAND)
+            count_documents = utils.DB_TERMINAL_PLACES.count_documents({"name": processed_default})
+            if count_documents:
+                matches = utils.DB_TERMINAL_PLACES.find(
+                    {"name": processed_default,
+                     "location": {"$near": {"$geometry": location}}})
+
+        # Should have matches at this point, but if we haven't, return empty list
+        if not matches:
+            return []
+
+        # find the closest matched brand with activity
+        print("Found matches. Getting the closest match with activity...")
+        base_brand = first_with_activity(matches)
+
+        # find nearby competitive retail site
+        print("Finding a nearby competitive retail site.")
+        comparison_brand = find_nearby_competitor_with_activity(base_brand['name'], base_brand['type'], location)
+        category = base_brand['type']
+        closest_county = list(utils.DB_REGIONS.find({"type": "county", "geometry": {
+            "$near": {"$geometry": location, "$maxDistance": 1}}}))[0]['name'] # TODO: may need to error check if counties are blank
+
+        # add to query list
+        print("Adding desired tenant queries")
+        print(base_brand)
+        print(comparison_brand)
+        print(category)
+        print(closest_county)
+        query_list.append(['ACTIVITY', base_brand['name'], base_brand['address']])
+        if comparison_brand: query_list.append(['ACTIVITY', comparison_brand['name'], comparison_brand['address']])
+        query_list.append(['ACTIVITY', category, closest_county])
+        query_list.append(['PERFORMANCE', base_brand['name'], base_brand['address']])
+        if comparison_brand: query_list.append(['PERFORMANCE', comparison_brand['name'], comparison_brand['address']])
+        query_list.append(['PERFORMANCE', category, closest_county])
+
+        return query_list
+
+    # if type landlord
+    if ('owner' or 'hospitality industry' or 'shopping center management') in type:
+        NUM_CENTER_COMPS = 4
+
+        # find closest retail with activity in local vicinity
+        print("Finding closest retail site for user location")
+        matches = utils.DB_TERMINAL_PLACES.find(
+            {"location": {"$near": {"$geometry": location, "$maxDistance": SEARCH_RADIUS}}})
+        if not matches:
+            print("No retail found in area for {}, {}".format(address, city))
+            return []
+
+        base_brand = first_with_activity(matches)
+
+        # find the retail near initial brand
+        near_base_matches = utils.DB_TERMINAL_PLACES.find(
+            {"location": {"$near": {"$geometry": base_brand['location'], "$maxDistance": SEARCH_RADIUS}}})
+        nearby_brands = first_with_activity(near_base_matches, NUM_CENTER_COMPS)
+
+        comp_brand = most_likely_chain(nearby_brands)
+        comp_brand_county = list(utils.DB_REGIONS.find({"type": "county", "geometry": {
+            "$near": {"$geometry": comp_brand['location'], "$maxDistance": 1}}}))[0][
+            'name']  # TODO: may need to error check if counties are blank
+
+        # TODO: if no retail is found in the near vicinity, choose retail in the closest MSA
+
+        # add to query list
+        print("Adding desired tenant queries")
+        print(base_brand)
+        query_list.append(['ACTIVITY', comp_brand['name'], comp_brand['address']])
+        query_list.append(['ACTIVITY', comp_brand['name'], comp_brand_county])
+        query_list.append(['ACTIVITY', comp_brand['type'], comp_brand_county])
+        query_list.append(['PERFORMANCE', comp_brand['name'], comp_brand['address']])
+        query_list.append(['PERFORMANCE', comp_brand['name'], comp_brand_county])
+        query_list.append(['PERFORMANCE', comp_brand['type'], comp_brand_county])
+
+        # TODO: allow for break in performance tables
+
+        for item in nearby_brands:
+            query_list.append(['PERFORMANCE', item['name'], item['address']])
+
+        return query_list
+
+    # if type broker
+    if ('real estate services' or 'retail broker' or 'tenant services') in type:
+        return None
+
+    # if type municipality
+    if 'public sector' in type:
+        return None
+
+    # if type scholar
+    if ('academic institution' or 'student') in type:
+        return None
+
+    # if type people researcher
+    if ('Consumer/Market Research'.lower() or 'Financial/Investment Services'.lower() or 'Parking and Traffic'.lower()) in type:
+        return None
+
+    # if type services
+    if ('advertising/marketing/pr' or 'architecture/design/engineering' or 'Building Materials/Structural'.lower() or
+        'Computer Software/Hardware'.lower() or 'Construction'.lower() or 'insurance' or 'law firm' or
+        'lending institution' or 'maintenance' or 'other business services' or 'personell services' or 'press/media' or
+        'publications/publishers' or 'trade association' or 'utilities/telecommunication') in type:
+        return None
+
+    # TODO check to see if city is something within our viewports
+
+    return None
+
+def find_nearby_competitor_with_activity(brand, category, location):
+
+    # find largest nearby competitors
+    print("Finding the largest nearby competitors")
+    category_matches = utils.DB_TERMINAL_PLACES.find({"type": category, "location": {
+        "$near": {"$geometry": location, "$maxDistance": SEARCH_RADIUS}}})
+    same_cat_brands = [match['name'] for match in category_matches if match['name'].lower() != brand.lower()]
+    most_likely_competitors = sorted(Counter(same_cat_brands).items(), key=lambda x:x[1], reverse=True) # TODO: make place matching better for name variations
+
+    # find closest large competitor with activity
+    print("Finding the closest large competitor with activity")
+    comp_with_activity = None
+    for competitor in most_likely_competitors:
+        if comp_with_activity:
+            return comp_with_activity
+        matches = list(utils.DB_TERMINAL_PLACES.find(
+            {"name": competitor[0], "location": {
+                "$near": {"$geometry": location, "$maxDistance": SEARCH_RADIUS}}}))
+        comp_with_activity = first_with_activity(matches)
+    return None
+
+def first_with_activity(matches, num_results=1):
+    # finds the first brand that has activity out of an iterable of brands, sorted by distance from predetermined location
+    results = []
+    for result in matches:
+        if len(results) >= num_results:
+            if len(results)==1: return results[0]
+            return results
+        if 'activity' in result and result['activity']:
+            results.append(result)
+    return None
+
+def get_coords(city, address=None):
+    url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={}&inputtype=textquery&fields=name,formatted_address,geometry&key={}'
+    if not address and isinstance(city, str):
+        result = requests.get(url.format(utils.encode_word(city), GOOG_KEY)).json()
+    elif (isinstance(address, str) and (isinstance(city, str))):
+        result = (requests.get(url.format(utils.encode_word(address + ', ' + city), GOOG_KEY)).json())
+    else:
+        return None
+    return result['candidates'][0]['geometry']['location'] if result['candidates'] else None
+
+def most_likely_chain(matches):
+    # finds the retailer that's most likely a chain the local area
+    most_likely = (None, 0)
+    for brand in matches:
+        results = utils.DB_TERMINAL_PLACES.find(
+            {"name": brand['name'], "location": {"$near": {"$geometry": brand['location'], "$maxDistance": 2*SEARCH_RADIUS}}})
+        result_list = list(results)
+        if len(result_list) > most_likely[1]:
+            most_likely = (brand, len(result_list))
+
+    return most_likely[0]
+
+if __name__ == "__main__":
+    def test_find_competitor_with_activity():
+        brand = "Starbucks"
+        category = "Coffee Shop"
+        location = {'type': 'Point', 'coordinates': [-122.250173, 47.41283]}
+        print(find_nearby_competitor_with_activity(brand, category, location))
+
+    # test_find_competitor_with_activity()
+    filename = "/Users/colin/Downloads/icsc_emails_short_owner.csv"
+    personal_reports(filename)
+
