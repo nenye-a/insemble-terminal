37b5cae34165f3b628c67144cceec5fda15a761a
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-25 22:57:21 +0700

    fix timezone

--- frontend/src/helpers/__tests__/getPublishedDate-test.ts
+++ frontend/src/helpers/__tests__/getPublishedDate-test.ts
@@ -5,7 +5,7 @@ describe('getPublishedDate', () => {
     let arr = ['2020-05-28T11:03:00.000Z', '2020-05-22T11:03:00.000Z'];
 
     let convertedArr = arr.map((item) =>
-      getPublishedDate(item, 1590748938818, false),
+      getPublishedDate(item, new Date(2020, 4, 29, 17, 43)),
     ); // May-29-2020:5:43
     expect(convertedArr).toEqual(['23 hours ago', '5/22']);
   });
--- frontend/src/helpers/getPublishedDate.ts
+++ frontend/src/helpers/getPublishedDate.ts
@@ -1,33 +1,27 @@
-export default function getPublishedDate(
-  date: string,
-  now = Date.now(),
-  needParsing = true,
-) {
+export default function getPublishedDate(date: string, now = new Date()) {
   if (date) {
-    // to handle the Safari invalid date
-    let dateParamString = needParsing ? date.replace(/-/g, '/') : date;
+    let dateParam = new Date(date);
     let dateNow = new Date(now);
-    let dateParam = new Date(dateParamString);
-
-    let difference =
-      Number(dateNow) -
-      Number(dateNow.getTimezoneOffset() * 60 * 1000) -
-      Number(dateParam);
-
+    let difference = Number(dateNow) - Number(dateParam);
     let oneDay = 24 * 60 * 60 * 1000;
     let oneHour = 60 * 60 * 1000;
     let oneMinute = 60 * 1000;
 
-    if (difference > oneDay) {
+    if (difference < 0) {
+      // handle minus diff
       return `${dateParam.getUTCMonth() + 1}/${dateParam.getUTCDate()}`;
     } else {
-      let hour = Math.floor(difference / 3600000);
-      if (difference > oneHour) {
-        return `${hour} hours ago`;
-      } else if (difference > oneMinute) {
-        return `${Math.floor(difference / 60000)} minutes ago`;
+      if (difference > oneDay) {
+        return `${dateParam.getUTCMonth() + 1}/${dateParam.getUTCDate()}`;
       } else {
-        return `${Math.floor(difference / 1000)} seconds ago`;
+        let hour = Math.floor(difference / 3600000);
+        if (difference > oneHour) {
+          return `${hour} hours ago`;
+        } else if (difference > oneMinute) {
+          return `${Math.floor(difference / 60000)} minutes ago`;
+        } else {
+          return `${Math.floor(difference / 1000)} seconds ago`;
+        }
       }
     }
   }
