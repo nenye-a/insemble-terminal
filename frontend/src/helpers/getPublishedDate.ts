a6dbd3cb2fe5719add04f519bab06baf29e8afad
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-06-25 17:44:06 +0700

    fix timezone offset on published date

--- frontend/src/helpers/getPublishedDate.ts
+++ frontend/src/helpers/getPublishedDate.ts
@@ -2,14 +2,17 @@ export default function getPublishedDate(date: string, now = Date.now()) {
   if (date) {
     let dateNow = new Date(now);
     let dateParam = new Date(date);
-    let difference = Number(dateNow) - Number(dateParam);
+    let difference =
+      Number(dateNow) -
+      Number(dateNow.getTimezoneOffset() * 60 * 1000) -
+      Number(dateParam);
 
     let oneDay = 24 * 60 * 60 * 1000;
     let oneHour = 60 * 60 * 1000;
     let oneMinute = 60 * 1000;
 
     if (difference > oneDay) {
-      return `${dateParam.getMonth() + 1}/${dateParam.getDate()}`;
+      return `${dateParam.getUTCMonth() + 1}/${dateParam.getUTCDate()}`;
     } else {
       let hour = Math.floor(difference / 3600000);
       if (difference > oneHour) {
