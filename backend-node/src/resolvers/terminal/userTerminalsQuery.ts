321495c72de73872a34f68de874e71b20949a259
Raymond Anggara <46436058+RaymondAnggara@users.noreply.github.com>
2020-07-07 12:23:21 +0700

    [Functional] Terminal pagination (#414)
    * Backend-node pagination terminal
    
    * Py BE gql query adjust
    
    * Add react paginate
    
    * Manual state fetchMore
    
    * Fix page pointer broken
    
    * Make on search point back to page 1(0)
    
    * Remove unused
    
    * Fix If expression
    
    * Fix Mobile pagination
    
    * Fix suggestion

--- backend-node/src/resolvers/terminal/userTerminalsQuery.ts
+++ backend-node/src/resolvers/terminal/userTerminalsQuery.ts
@@ -1,10 +1,10 @@
-import { queryField, FieldResolver } from 'nexus';
+import { queryField, FieldResolver, intArg, stringArg } from 'nexus';
 
 import { Root, Context } from 'serverTypes';
 
 let userTerminalsResolver: FieldResolver<'Query', 'userTerminals'> = async (
   _: Root,
-  _args,
+  { search, first, skip },
   context: Context,
 ) => {
   let terminals = await context.prisma.terminal.findMany({
@@ -17,12 +17,29 @@ let userTerminalsResolver: FieldResolver<'Query', 'userTerminals'> = async (
       createdAt: 'asc',
     },
   });
+  if (search) {
+    terminals = terminals.filter(({ name }) =>
+      name.toLowerCase().includes(search.toLowerCase()),
+    );
+  }
+  if (skip) {
+    terminals = terminals.filter((_, index) => index >= skip);
+  }
+  if (first) {
+    terminals = terminals.filter((_, index) => index < first);
+  }
+
   return terminals;
 };
 
 let userTerminals = queryField('userTerminals', {
   type: 'Terminal',
   list: true,
+  args: {
+    search: stringArg(),
+    first: intArg(),
+    skip: intArg(),
+  },
   resolve: userTerminalsResolver,
 });
 
