685d08f99b2f906cf710a268eeb5c23064a0c8cd
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-07-07 15:43:54 +0700

    add background

---
+++ frontend/src/components/Background.tsx
@@ -0,0 +1,43 @@
+import React, { SVGProps } from 'react';
+import styled from 'styled-components';
+
+import { View } from '../core-ui';
+import { NAVBAR_HEIGHT } from '../constants/theme';
+import { LIGHT_PURPLE } from '../constants/colors';
+import withBubbleBackground from '../assets/images/curve-background-bubble.svg';
+import halfPurpleBackground from '../assets/images/curve-background.svg';
+
+const BG_SOURCE = {
+  halfPurple: halfPurpleBackground,
+  quarterPurple: halfPurpleBackground,
+  withBubble: withBubbleBackground,
+};
+
+type Props = ViewProps & {
+  mode?: keyof typeof BG_SOURCE;
+};
+
+const BackgroundBubble = ({
+  mode = 'halfPurple',
+  children,
+  ...props
+}: Props) => (
+  <Container {...props}>
+    <Img src={BG_SOURCE[mode]} />
+    {children}
+  </Container>
+);
+
+const Container = styled(View)`
+  background-color: ${LIGHT_PURPLE};
+  overflow: hidden;
+`;
+const Img = styled.img`
+  position: absolute;
+  bottom: -60px;
+  object-fit: cover;
+  height: 100%;
+  z-index: 0;
+`;
+
+export default BackgroundBubble;
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Background } from './Background';
 export { default as DataTable } from './DataTable';
 export { default as ComparisonPopover } from './ComparisonPopover';
 export { default as EmptyDataComponent } from './EmptyDataComponent';
