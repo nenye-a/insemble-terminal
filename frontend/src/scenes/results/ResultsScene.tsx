9ffc2feb0998bd210210855809a734af3391e2cd
Nenye <57047007+nenye-a@users.noreply.github.com>
2020-07-13 08:51:02 -0700

    keep tutorial hidden until it's ready for release

--- frontend/src/scenes/results/ResultsScene.tsx
+++ frontend/src/scenes/results/ResultsScene.tsx
@@ -8,7 +8,7 @@ import {
   HeaderNavigationBar,
   PageTitle,
   SearchPlaceholder,
-  TutorialButton,
+  // TutorialButton,
 } from '../../components';
 import { MUTED_TEXT_COLOR, BACKGROUND_COLOR } from '../../constants/colors';
 import { SEARCH, GET_SEARCH_TAG } from '../../graphql/queries/server/search';
@@ -241,9 +241,9 @@ export default function ResultsScene() {
             locationTag={selectedSearchTagWithIds.locationTag}
           />
           <Container isDesktop={isDesktop}>
-            {isDesktop && (
+            {/* {isDesktop && (
               <TutorialButton style={{ position: 'absolute', right: 36 }} />
-            )}
+            )} */}
             {resultQueries.map(({ reviewTag, type }, idx) => {
               let key = `${reviewTag}-${type}-${idx}`;
               let props = {
