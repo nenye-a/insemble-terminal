52115f4c7118fb05634dfb8bd5b238f7d5f65bf8
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-07-13 17:10:39 +0700

    fix contacts navigation

--- frontend/src/scenes/tutorial/PersonalTerminalContent.tsx
+++ frontend/src/scenes/tutorial/PersonalTerminalContent.tsx
@@ -64,7 +64,7 @@ export default function PersonalTerminalContent() {
         leftButton={{
           text: 'Contact',
           onPress: () => {
-            onPageChange('contact');
+            onPageChange('contacts');
           },
         }}
       />
