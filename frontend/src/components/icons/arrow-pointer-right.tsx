de84f735e1d226ba534731da9e69d831fbf8ea02
Nathania Elvina <nathaniaelvina@hotmail.com>
2020-07-13 17:12:48 +0700

    [Scene] Terminal Tutorial (#462)

---
+++ frontend/src/components/icons/arrow-pointer-right.tsx
@@ -0,0 +1,32 @@
+import React, { SVGProps } from 'react';
+
+const SvgArrowPointerRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="33.708"
+    height="24.369"
+    viewBox="0 0 33.708 24.369"
+    {...props}
+  >
+    <g
+      id="Group_1896"
+      data-name="Group 1896"
+      transform="translate(-541.658 696.369) rotate(-90)"
+    >
+      <g
+        id="noun_Arrow_2320225"
+        transform="translate(696.369 541.659) rotate(90)"
+      >
+        <path
+          id="Path_4010"
+          data-name="Path 4010"
+          d="M33.371,13.392a.2.2,0,0,0,.056-.165.412.412,0,0,0,.112-.22c0-.055.056-.11.056-.22,0-.055.056-.165.056-.22,0-.11.056-.22.056-.329v-.22a.682.682,0,0,0-.056-.329c0-.11-.056-.165-.056-.22s-.056-.11-.056-.165c-.056-.11-.112-.165-.169-.274,0-.055-.056-.055-.056-.11-.112-.11-.169-.22-.281-.329L22.866.659a2.276,2.276,0,0,0-3.2,0,2.152,2.152,0,0,0,0,3.128l6.348,6.2H2.247a2.2,2.2,0,1,0,0,4.391H26.012l-6.348,6.2a2.152,2.152,0,0,0,0,3.128,2.208,2.208,0,0,0,3.146,0l10.225-9.989.337-.329Z"
+          transform="translate(0)"
+          fill="currentColor"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default SvgArrowPointerRight;
