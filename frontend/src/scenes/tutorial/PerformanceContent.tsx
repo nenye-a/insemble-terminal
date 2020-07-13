02ffcfe30747a8a30461834cae0ba34b1796e60a
RaymondAnggara <raymond@kodefox.com>
2020-07-13 13:56:20 +0700

    Adjust PerformanceContent

--- frontend/src/scenes/tutorial/PerformanceContent.tsx
+++ frontend/src/scenes/tutorial/PerformanceContent.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Text as BaseText, View, Card } from '../../core-ui';
-import { FONT_WEIGHT_MEDIUM } from '../../constants/theme';
-import { DARK_TEXT_COLOR, SLIGHT_GRAY } from '../../constants/colors';
+import {
+  FONT_WEIGHT_MEDIUM,
+  DEFAULT_BORDER_RADIUS,
+} from '../../constants/theme';
+import {
+  DARK_TEXT_COLOR,
+  SLIGHT_GRAY,
+  SHADOW_COLOR,
+} from '../../constants/colors';
 import { useViewport } from '../../helpers';
 import { PerformanceTableType, DemoType } from '../../generated/globalTypes';
 import performanceSearchbarDemo from '../../assets/images/performance-searchbox-demo.svg';
 import SvgFourDotsArrow from '../../components/icons/four-dots-arrow';
+import { useTutorialContext } from '../../context';
 import PerformanceTablePopover from '../results/PerformanceTablePopover';
 import PerformanceResult from '../results/PerformanceResult';
 
+import BottomNavigation from './BottomNavigation';
+
 export default function PerformanceContent() {
   let { isDesktop } = useViewport();
+  let { onPageChange } = useTutorialContext();
+  let demonstrationText = (
+    <Text color={DARK_TEXT_COLOR} style={{ alignSelf: 'flex-end' }}>
+      * Data in this example data, for demonstration purposes only
+    </Text>
+  );
   return (
     <View>
       <Title>Performance</Title>
@@ -22,11 +38,9 @@ export default function PerformanceContent() {
         markets. We show you the performance of individual locations and also of
         the market as a whole.
       </Paragraph>
-      <img src={performanceSearchbarDemo} alt="" />
+      <SearchBar src={performanceSearchbarDemo} />
       <SvgFourDotsArrow
         style={{
-          marginBottom: 20,
-          marginTop: 20,
           alignSelf: 'center',
           color: SLIGHT_GRAY,
         }}
@@ -36,11 +50,13 @@ export default function PerformanceContent() {
         performanceType={PerformanceTableType.OVERALL}
         demoType={DemoType.BASIC}
       />
+      {demonstrationText}
       <PerformanceResult
         title="By Location"
         performanceType={PerformanceTableType.ADDRESS}
         demoType={DemoType.BASIC}
       />
+      {demonstrationText}
       <Paragraph>
         Insemble supports multiple indicators of performance.
       </Paragraph>
@@ -54,6 +70,7 @@ export default function PerformanceContent() {
         demoType={DemoType.BASIC}
         initialView="graph"
       />
+      {demonstrationText}
       {/* TODO: Scopes Tutorial */}
       <SubTitle style={{ paddingBottom: 26, paddingTop: 70 }}>
         Comparisons
@@ -66,6 +83,21 @@ export default function PerformanceContent() {
         title="Overall Performance"
         performanceType={PerformanceTableType.OVERALL}
         demoType={DemoType.WITH_COMPARE}
+      />
+      {demonstrationText}
+      <BottomNavigation
+        leftButton={{
+          text: 'Overview',
+          onPress: () => {
+            onPageChange('overview');
+          },
+        }}
+        rightButton={{
+          text: 'Activity',
+          onPress: () => {
+            onPageChange('activity');
+          },
+        }}
       />
     </View>
   );
@@ -93,4 +125,11 @@ const InfoboxContainer = styled(Card)`
   padding: 14px;
   margin: 15px 0px;
   align-self: center;
+`;
+
+const SearchBar = styled.img`
+  width: 100%;
+  border-radius: ${DEFAULT_BORDER_RADIUS};
+  box-shadow: ${SHADOW_COLOR};
+  margin: 33px 0;
 `;
