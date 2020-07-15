cd16fb3a12883147fd8a3e37861a4bc734ab23f2
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-07-15 14:25:17 +0700

    responsive contact table

--- frontend/src/scenes/results/ContactsTable.tsx
+++ frontend/src/scenes/results/ContactsTable.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Text, View } from '../../core-ui';
 import { DataTable } from '../../components';
 import { GetOwnershipContactData_ownershipContactTable_data as ContactData } from '../../generated/GetOwnershipContactData';
+import { useViewport } from '../../helpers';
 
 type Props = {
   data?: Array<ContactData>;
@@ -9,26 +11,58 @@ type Props = {
 
 export default function ContactsTable(props: Props) {
   let { data = [] } = props;
+  let { isDesktop } = useViewport();
+  if (isDesktop) {
+    return (
+      <DataTable>
+        <DataTable.HeaderRow>
+          <DataTable.HeaderCell width={260}>Name</DataTable.HeaderCell>
+          <DataTable.HeaderCell align="right">Title</DataTable.HeaderCell>
+          <DataTable.HeaderCell align="right">Phone</DataTable.HeaderCell>
+          <DataTable.HeaderCell width={260} align="right">
+            Email
+          </DataTable.HeaderCell>
+        </DataTable.HeaderRow>
+        <DataTable.Body>
+          {data.map((row, index) => {
+            let { name, email, phone, title } = row;
+            return (
+              <DataTable.Row key={index}>
+                <DataTable.Cell width={260}>{name}</DataTable.Cell>
+                <DataTable.Cell align="right">{title}</DataTable.Cell>
+                <DataTable.Cell align="right">{phone}</DataTable.Cell>
+                <DataTable.Cell width={260} align="right">
+                  {email}
+                </DataTable.Cell>
+              </DataTable.Row>
+            );
+          })}
+        </DataTable.Body>
+      </DataTable>
+    );
+  }
   return (
     <DataTable>
       <DataTable.HeaderRow>
         <DataTable.HeaderCell width={260}>Name</DataTable.HeaderCell>
-        <DataTable.HeaderCell align="right">Title</DataTable.HeaderCell>
-        <DataTable.HeaderCell align="right">Phone</DataTable.HeaderCell>
-        <DataTable.HeaderCell width={260} align="right">
-          Email
-        </DataTable.HeaderCell>
+        <DataTable.HeaderCell align="right">Contact</DataTable.HeaderCell>
       </DataTable.HeaderRow>
       <DataTable.Body>
         {data.map((row, index) => {
-          let { name, email, phone, title } = row;
+          let { name, email, phone, title = '-' } = row;
           return (
             <DataTable.Row key={index}>
-              <DataTable.Cell width={260}>{name}</DataTable.Cell>
-              <DataTable.Cell align="right">{title}</DataTable.Cell>
-              <DataTable.Cell align="right">{phone}</DataTable.Cell>
-              <DataTable.Cell width={260} align="right">
-                {email}
+              <DataTable.Cell width={260}>
+                <View>
+                  <Text>{name}</Text>
+                  <Text>({title})</Text>
+                </View>
+              </DataTable.Cell>
+              <DataTable.Cell align="right">
+                <View>
+                  <Text>{phone}</Text>
+                  <Text> {email}</Text>
+                </View>
               </DataTable.Cell>
             </DataTable.Row>
           );
--- frontend/src/scenes/results/OwnershipInformationCard.tsx
+++ frontend/src/scenes/results/OwnershipInformationCard.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import { View, Text, Link } from '../../core-ui';
 import { FONT_WEIGHT_MEDIUM, FONT_SIZE_XSMALL } from '../../constants/theme';
-import { GRAY, SHADOW_COLOR, WHITE } from '../../constants/colors';
+import { GRAY, SHADOW_COLOR, WHITE, LINK_COLOR } from '../../constants/colors';
 
 type Props = {
   name?: string;
@@ -37,7 +37,9 @@ export default function OwnershipInformationCard(props: Props) {
       </Row>
       <Row>
         <LabelText>Website</LabelText>
-        <Link href={website}>{website}</Link>
+        <Link href={website} style={{ color: LINK_COLOR }}>
+          {website}
+        </Link>
       </Row>
       <LastUpdateText>
         Last Updated: {new Date(lastUpdate).toString()}
