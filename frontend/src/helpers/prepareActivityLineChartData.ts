24c6e9ba2aba059568c7ec6560c04e11c9986bb7
nathaniaelvina <nathaniaelvina@hotmail.com>
2020-07-20 10:17:01 +0700

    activity csv data

---
+++ frontend/src/helpers/prepareActivityLineChartData.ts
@@ -0,0 +1,44 @@
+import { COLORS } from '../constants/colors';
+
+import generateRandomColor from './generateRandomColor';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ChartDatum = { [key: string]: any };
+
+type TooltipWithFill = {
+  label: string;
+  fill: string;
+};
+
+export default function prepareActivityLineChartData(
+  rawData: Array<Array<ChartDatum>>,
+  xAxis: string,
+  yAxis: string,
+  tooltipInfo: string,
+): {
+  tooltipData: Array<TooltipWithFill>;
+  lineChartData: Array<ChartDatum>;
+} {
+  let x = xAxis;
+  let y = yAxis;
+  let lineChartData: ChartDatum = {};
+  let tooltipData = [];
+  for (let data of rawData) {
+    for (let datum of data) {
+      tooltipData.push(`${datum[tooltipInfo]}`);
+      lineChartData[datum[x]] = {
+        ...lineChartData[datum[x]],
+        ...datum,
+        [`${datum[tooltipInfo]}`]: datum[y],
+      };
+    }
+  }
+  let tooltipWithFill = [...new Set(tooltipData)].map((tooltip, idx) => ({
+    label: tooltip,
+    fill: COLORS[idx] || generateRandomColor(),
+  }));
+  return {
+    tooltipData: tooltipWithFill,
+    lineChartData: Object.values(lineChartData),
+  };
+}
